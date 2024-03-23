c7adb4b56f5270ac06dbbfe38fb43006a3c92311
mrskiro <mrskiro.h@gmail.com>
2024-03-23 21:47:56 +0900

    fix: one book

--- workspaces/app/src/features/book/components/BookCard.tsx
+++ workspaces/app/src/features/book/components/BookCard.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from 'react';
 import { styled } from 'styled-components';
 
 import { Flex } from '../../../foundation/components/Flex';
@@ -7,7 +6,6 @@ import { Link } from '../../../foundation/components/Link';
 import { Text } from '../../../foundation/components/Text';
 import { useImage } from '../../../foundation/hooks/useImage';
 import { Color, Radius, Space, Typography } from '../../../foundation/styles/variables';
-import { useBook } from '../hooks/useBook';
 
 const _Wrapper = styled(Link)`
   display: flex;
@@ -33,17 +31,28 @@ const _AvatarWrapper = styled.div`
 `;
 
 type Props = {
-  bookId: string;
+  book: {
+    author: {
+      image: {
+        id: string;
+      };
+      name: string;
+    };
+    id: string;
+    image: {
+      alt: string;
+      id: string;
+    };
+    name: string;
+  };
 };
 
-const BookCard: React.FC<Props> = ({ bookId }) => {
-  const { data: book } = useBook({ params: { bookId } });
-
+const BookCard: React.FC<Props> = ({ book }) => {
   const imageUrl = useImage({ height: 128, imageId: book.image.id, width: 192 });
   const authorImageUrl = useImage({ height: 32, imageId: book.author.image.id, width: 32 });
 
   return (
-    <_Wrapper href={`/books/${bookId}`}>
+    <_Wrapper href={`/books/${book.id}`}>
       {imageUrl != null && (
         <_ImgWrapper>
           <Image alt={book.image.alt} height={128} objectFit="cover" src={imageUrl} width={192} />
@@ -72,9 +81,9 @@ const BookCard: React.FC<Props> = ({ bookId }) => {
 
 const BookCardWithSuspense: React.FC<Props> = (props) => {
   return (
-    <Suspense fallback={null}>
-      <BookCard {...props} />
-    </Suspense>
+    // <Suspense fallback={null}>
+    <BookCard {...props} />
+    // </Suspense>
   );
 };
 
--- workspaces/app/src/features/ranking/components/RankingCard.tsx
+++ workspaces/app/src/features/ranking/components/RankingCard.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from 'react';
 import styled from 'styled-components';
 
 import { SvgIcon } from '../../../features/icons/components/SvgIcon';
@@ -11,7 +10,6 @@ import { Spacer } from '../../../foundation/components/Spacer';
 import { Text } from '../../../foundation/components/Text';
 import { useImage } from '../../../foundation/hooks/useImage';
 import { Color, Radius, Space, Typography } from '../../../foundation/styles/variables';
-import { useBook } from '../../book/hooks/useBook';
 
 const _Wrapper = styled.li`
   width: 100%;
@@ -38,12 +36,23 @@ const _AvatarWrapper = styled.div`
 `;
 
 type Props = {
-  bookId: string;
+  book: {
+    author: {
+      image: {
+        id: string;
+      };
+      name: string;
+    };
+    description: string;
+    id: string;
+    image: {
+      id: string;
+    };
+    name: string;
+  };
 };
 
-const RankingCard: React.FC<Props> = ({ bookId }) => {
-  const { data: book } = useBook({ params: { bookId } });
-
+const RankingCard: React.FC<Props> = ({ book }) => {
   const imageUrl = useImage({ height: 96, imageId: book.image.id, width: 96 });
   const authorImageUrl = useImage({ height: 32, imageId: book.author.image.id, width: 32 });
 
@@ -105,9 +114,9 @@ const RankingCard: React.FC<Props> = ({ bookId }) => {
 
 const RankingCardWithSuspense: React.FC<Props> = (props) => {
   return (
-    <Suspense fallback={null}>
-      <RankingCard {...props} />
-    </Suspense>
+    // <Suspense fallback={null}>
+    <RankingCard {...props} />
+    // </Suspense>
   );
 };
 
