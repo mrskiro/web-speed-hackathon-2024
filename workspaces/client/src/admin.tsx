a1ee5f2c4ae9bd5aa884fed1f0871d436f12f225
mrskiro <mrskiro.h@gmail.com>
2024-03-24 09:13:36 +0900

    Revert "fix admin worker"
    This reverts commit eebefda7c9167f0d10fc49d343bc29d4e974da89.

--- workspaces/client/src/admin.tsx
+++ workspaces/client/src/admin.tsx
@@ -2,7 +2,12 @@ import ReactDOM from 'react-dom/client';
 
 import { AdminApp } from '@wsh-2024/admin/src/index';
 
+// import { preloadImages } from './utils/preloadImages';
+import { registerServiceWorker } from './utils/registerServiceWorker';
+
 const main = async () => {
+  await registerServiceWorker();
+  // await preloadImages();
   const root = document.getElementById('root');
 
   ReactDOM.createRoot(root!).render(<AdminApp />);
